98acf9df76970c4fdb8ea8610ae4731fa478ce05
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-21 22:07:52 +0400

    обновлен

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,38 @@
-import vk
+import requests
+import json
+import time
+from misc import TOKEN
+from misc import OWNER_ID
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
-
-def get_user_login():
-    pass
-
-
-def get_user_password():
-    pass
-
-
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
+def get_friends_id(token, owner_id):
+    parameters = {"access_token": token, "owner_id": owner_id, "v": "5.75"}
+    id_response = requests.get(
+        "https://api.vk.com/method/friends.getOnline",
+        params=parameters
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    return id_response.json()["response"]
 
 
-def output_friends_to_console(friends_online):
-    pass
+def print_friends_name(friends_id, token):
+    for friend_id in friends_id:
+        parameters = {
+            "v": "5.75",
+            "access_token": token,
+            "fields": "bdate",
+            "user_ids": friend_id
+        }
+        friends_name = requests.get(
+            "https://api.vk.com/method/users.get",
+            params=parameters
+        )
+        time.sleep(2)
+        print(
+            friends_name.json()["response"][0]["first_name"],
+            friends_name.json()["response"][0]["last_name"]
+        )
 
-if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+
+if __name__ == "__main__":
+    friends_id = get_friends_id(TOKEN, OWNER_ID)
+    print_friends_name(friends_id, TOKEN)
