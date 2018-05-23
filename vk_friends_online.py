703f89a61c279ef48d89c2b1c85740cf631d32af
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-23 18:27:45 +0300

    добавлен ввод логина и пароля

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,37 +1,53 @@
-import requests
-import json
-import time
-from misc import TOKEN
-from misc import OWNER_ID
+import vk
+import getpass
 
 
-def get_friends_id(token, owner_id):
-    parameters = {"access_token": token, "owner_id": owner_id, "v": "5.75"}
-    id_response = requests.get(
-        "https://api.vk.com/method/friends.getOnline",
-        params=parameters
+APP_ID = "your app_id"
+
+
+def get_user_login():
+    login = input("Введите логин: ")
+    return login
+
+
+def get_user_password():
+    password = getpass.getpass("Введите пароль: ")
+    return password
+
+
+def get_user_id(version=5.75):
+    user_id = vk_api.users.get(v=version)
+    return str(user_id[0]["id"])
+
+
+def get_online_friends_id(user_id, version=5.75):
+    friends_id = vk_api.friends.getOnline(user_id=user_id, v=version)
+    return friends_id
+
+
+def get_online_friends(friends_id, version=5.75):
+    friends_online = vk_api.users.get(user_ids=friends_id, v=version)
+    return friends_online
+
+
+def output_friends_to_console(friends_online, online_now):
+    print(online_now)
+    for friend_online in friends_online:
+        print(friend_online["first_name"], friend_online["last_name"])
+
+
+if __name__ == '__main__':
+    login = get_user_login()
+    password = get_user_password()
+    session = vk.AuthSession(
+        app_id=APP_ID,
+        user_login=login,
+        user_password=password,
+        scope="friends"
     )
-    return id_response.json()["response"]
+    vk_api = vk.API(session)
+    user_id = get_user_id()
+    friends_id = get_online_friends_id(user_id)
+    friends_online = get_online_friends(friends_id)
+    output_friends_to_console(friends_online, "Сейчас онлайн:")
 
-
-def print_friends_name(friends_id, token):
-    for friend_id in friends_id:
-        parameters = {
-            "v": "5.75",
-            "access_token": token,
-            "fields": "bdate",
-            "user_ids": friend_id
-        }
-        friends_name = requests.get(
-            "https://api.vk.com/method/users.get",
-            params=parameters
-        )
-        print(
-            friends_name.json()["response"][0]["first_name"],
-            friends_name.json()["response"][0]["last_name"]
-        )
-
-
-if __name__ == "__main__":
-    friends_id = get_friends_id(TOKEN, OWNER_ID)
-    print_friends_name(friends_id, TOKEN)
